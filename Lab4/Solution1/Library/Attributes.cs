47f5498862aefcc62a12bbdf27119c4d2f1cf2bd
maksimbelaev <maxim.beliaev18@gmail.com>
2026-04-23 16:28:47 +0300

    Lab4 initial

---
+++ Lab4/Solution1/Library/Attributes.cs
@@ -0,0 +1,30 @@
+namespace Library;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class MyTestClassAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class MyTestAttribute : Attribute 
+{
+    public string? Skip { get; set; } 
+}
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public class MyTestCaseAttribute : Attribute 
+{
+    public object[] Params { get; }
+    public MyTestCaseAttribute(params object[] parameters) => Params = parameters;
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class MyBeforeTestAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class MyAfterTestAttribute : Attribute { }
+
+[AttributeUsage(AttributeTargets.Method)]
+public class MyTestTimeoutAttribute : Attribute 
+{
+    public int Milliseconds { get; }
+    public MyTestTimeoutAttribute(int ms) => Milliseconds = ms;
+}
